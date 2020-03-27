da61af33f5181839c04927a5ac15849548a75adc
Tristan <tristan.sebens@Wostmann.com>
2020-03-27 14:01:47 -0800

    Added function to update JHU daily case directory

--- update.py
+++ update.py
@@ -4,23 +4,31 @@ Updates all relevant files in the repo.
 from urllib.request import urlopen
 import tempfile
 import os
+import shutil
 from zipfile import ZipFile
 
 _JHU_MASTER = r'https://github.com/CSSEGISandData/COVID-19/archive/master.zip'
 _DF = r'http://www.bay12games.com/dwarves/df_47_04_win.zip'
 
 
-def download_zip_and_unpack_to_dir(dir, zip_url):
-    with open(os.path.join(dir, 'temp.zip'), 'wb') as zip:
+def download_zip_and_unpack_to_dir(zip_url, _dir):
+    with open(os.path.join(_dir, 'temp.zip'), 'wb') as zip:
         zip.write(urlopen(zip_url).read())
-    with ZipFile(os.path.join(dir, 'temp.zip')) as zf:
-        zf.extractall(path=os.path.join(dir))
-    return dir
+    with ZipFile(os.path.join(_dir, 'temp.zip')) as zf:
+        zf.extractall(path=os.path.join(_dir))
+    return _dir
 
 
-new_temp_dir = tempfile.TemporaryDirectory()
-print(new_temp_dir.name)
-d = download_zip_and_unpack_to_dir(new_temp_dir.name, _JHU_MASTER)
-daily_reports_dir = os.path.join(new_temp_dir.name, 'COVID-19-master', 'csse_covid_19_data', 'csse_covid_19_daily_reports')
-print(daily_reports_dir)
-print(d)
+def update_jhu_data():
+    new_temp_dir = tempfile.TemporaryDirectory()
+    download_zip_and_unpack_to_dir(_JHU_MASTER, new_temp_dir.name)
+    jhu_repo_daily_reports_dir = os.path.join(new_temp_dir.name, 'COVID-19-master', 'csse_covid_19_data', 'csse_covid_19_daily_reports')
+    local_daily_reports_dir = os.path.join('data', 'cases-jhu', 'csse_covid_19_daily_reports')
+    # Copy the daily reports into the local directory
+    for f in os.listdir(jhu_repo_daily_reports_dir):
+        shutil.copyfile(
+            os.path.join(jhu_repo_daily_reports_dir, f),
+            os.path.join(local_daily_reports_dir, f)
+        )
+
+update_jhu_data()
